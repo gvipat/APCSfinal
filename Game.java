a2526d4745640e23dee4d783d201c17d04bbcca0
rosh324 <rosh324@users.noreply.github.com>
2018-04-24 19:43:10 -0700

    The main method has a title menu now, and calls level 1 when play is clicked. Levels now have a system that will be explained in class.

--- Game.java
+++ Game.java
@@ -1,11 +1,36 @@
+import java.awt.event.*;
+
+import javax.swing.*;
 
 public class Game
 {
-
     public static void main( String[] args )
     {
-        // TODO Auto-generated method stub
-
+        openTitleScreen();                
+    }
+    
+    private static void playGame()
+    {
+        
+    }
+    
+    private static void openTitleScreen()
+    {
+        final JFrame window = new JFrame("Super Cool Game");
+        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        JButton playButton = new JButton("Play");
+        playButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+               playGame();
+            }
+        });
+        window.add( playButton );
+        
+        
+        
+        window.setVisible( true );
     }
 
 }
--- Level.java
+++ Level.java
@@ -1,5 +1,27 @@
 
 public class Level
 {
-
+    private static final Sprite[][] sprites = {};
+    private static int level = 0;
+    
+    public Level()
+    {
+        //initialize the new controller here (possibly window too)
+    }
+    
+    public Level(Level last)
+    {
+        //same as default constructor but closes out the previous level
+    }
+    
+    public void play()
+    {
+        
+    }
+    
+    private void nextLevel()
+    {
+        level++;
+        Level next = new Level(this);
+    }
 }
