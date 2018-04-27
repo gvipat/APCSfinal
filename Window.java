fc88716ca51e920dcc388a671fc9858ef22600e3
gaura <gaura@DESKTOP-PAB173H>
2018-04-27 11:14:57 -0700

    commented out issues with window update image

--- Window.java
+++ Window.java
@@ -21,7 +21,7 @@ public class Window extends JFrame
     
     public void updaeImage(Color[][] pixels)
     {
-        image.setImage( new Image() );
+        //image.setImage( new Image() );
     }
     
     
