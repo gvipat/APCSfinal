4987f5a8c62f6392d2633f9f1c9c54f335e06a22
rosh324 <rosh324@users.noreply.github.com>
2018-04-27 11:45:32 -0700

    jpanel

--- Window.java
+++ Window.java
@@ -1,27 +1,13 @@
-import java.awt.Color;
-import java.awt.Image;
+import java.awt.*;
+import javax.swing.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-public class Window extends JFrame
-{    
-    private ImageIcon image = new ImageIcon();
-    
-    private JLabel label = new JLabel(image);
+public class Window extends JPanel
+{        
     
     public Window(int height, int width)
     {
         super.setSize( width, height );
-        super.setTitle( "Level " + Level.level );
         super.setVisible( true );
-        super.add( label );
-    }
-    
-    public void updateImage(Color[][] pixels)
-    {
-        //image.setImage( new Image() );
     }
     
     
