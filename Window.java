d6101bb18fa47506c0f44b19aad2703995a46dc8
rosh324 <rosh324@users.noreply.github.com>
2018-04-27 09:47:15 -0700

    window class

---
+++ Window.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Window extends JFrame
+{    
+    private ImageIcon image = new ImageIcon();
+    
+    private JLabel label = new JLabel(image);
+    
+    public Window(int height, int width)
+    {
+        super.setSize( width, height );
+        super.setTitle( "Level " + Level.level );
+        super.setVisible( true );
+        super.add( label );
+    }
+    
+    public void updaeImage(Color[][] pixels)
+    {
+        image.setImage( new Image() );
+    }
+    
+    
+    
+}
