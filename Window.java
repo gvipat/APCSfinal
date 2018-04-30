10cf206f86b6a904255e79d1aab32f79d0ef018e
rosh324 <rosh324@users.noreply.github.com>
2018-04-30 11:30:21 -0700

    yeeeeeeee

--- Window.java
+++ Window.java
@@ -10,25 +10,27 @@ public class Window extends JPanel
     public Window(int height, int width, PriorityQueue<Sprite> sprites)
     {
         frame = new JFrame("Level " + (Level.level + 1));
+        this.setPreferredSize(new Dimension(width, height));
+        super.setBackground(Color.BLACK);
         frame.add(this);
-        frame.setSize( width + 16, height + 38 );
+        frame.pack();
+        super.setVisible( true );
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println(getSize().toString());
         System.out.println( super.getSize().toString() );
         this.sprites = sprites;
-        super.setSize( width, height );
-        super.setBackground(Color.BLACK);
-        super.setVisible( true );
     }
     
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        for (Sprite s : sprites)
-        {
-            g.setColor(s.getColor());
-            g.drawRect(s.getX() - Engine.camera, s.getY() - Engine.camera,
-                s.getWidth(), s.getHeight());
-        }
+        // for (Sprite s : sprites)
+        // {
+        //     g.setColor(s.getColor());
+        //     g.drawRect(s.getX() - Engine.camera, s.getY() - Engine.camera,
+        //         s.getWidth(), s.getHeight());
+        // }
     }
     
     
