8554dd0d8bcf0fbdafbb97fe057e2321c9b69e08
rosh324 <rosh324@users.noreply.github.com>
2018-04-29 19:50:55 -0700

    fixed window to have a jframe

--- Window.java
+++ Window.java
@@ -5,12 +5,18 @@ import javax.swing.*;
 public class Window extends JPanel
 {        
     private PriorityQueue<Sprite> sprites;
+    private JFrame frame;
 
     public Window(int height, int width, PriorityQueue<Sprite> sprites)
     {
+        frame = new JFrame("Level " + (Level.level + 1));
+        frame.add(this);
+        frame.setSize( width + 16, height + 38 );
+        frame.setVisible(true);
+        System.out.println( super.getSize().toString() );
         this.sprites = sprites;
         super.setSize( width, height );
-        super.setBackground(Color.WHITE);
+        super.setBackground(Color.BLACK);
         super.setVisible( true );
     }
     
