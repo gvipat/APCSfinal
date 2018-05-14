2ec73809257d6110507b9175af06b50ccdc66efe
rosh324 <rosh324@users.noreply.github.com>
2018-05-13 18:56:46 -0700

    more junit stuff

--- JUGame.java
+++ JUGame.java
@@ -1,10 +1,66 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
 public class JUGame
 {
-    //Level Tests
+    // Level Tests
+
+    private Level level;
+    
     @Test
     public void levelConstructor()
     {
-        Level level = new Level();
-        assertNotNull(level);
+        level = new Level();
+        assertNotNull( level );
+    }
+
+
+    @Test
+    public void levelContructorParameter()
+    {
+        level = new Level( level );
+        assertNotNull( level );
+    }
+
+
+    @Test
+    public void levelPlayTest()
+    {
+
+    }
+
+
+    @Test
+    public void levelGetSpritesTest()
+    {
+        assertNotNull(level.getSprites());
+    }
+    
+    @Test
+    public void levelNextLevel()
+    {
+        int current = Level.level;
+        level.nextLevel();
+        assertTrue(Level.level == current + 1);
+    }
+    
+    
+    //Engine Tests
+    
+    private Engine engine;
+    
+    @Test
+    public void engineContructor()
+    {
+        engine = new Engine(level);
+        assertNotNull(engine);
+    }
+    
+    @Test
+    public void engineRunTest()
+    {
+        engine.run();
     }
 }
