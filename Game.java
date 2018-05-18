bad37100095619f943f8eb9f09ba4731f3ee869a
rosh324 <rosh324@users.noreply.github.com>
2018-05-18 10:51:49 -0700

    more javadocs

--- Game.java
+++ Game.java
@@ -2,19 +2,46 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+/**
+ *  The game class opens a title screen and opens the game.
+ *
+ *  @author  Roshan Sevalia
+ *  @version May 18, 2018
+ *  @author  Period: 4
+ *  @author  Assignment: APCSfinal
+ *
+ *  @author  Sources: Gaurav Vipat, Charlie Huang
+ */
 public class Game
 {
+    /**
+     * Main method
+     * @param args command line arguments (any argument causes the game to skip the title screen)
+     */
     public static void main( String[] args )
     {
-        openTitleScreen();                
+        if (args.length > 0)
+        {
+            playGame();
+        }
+        else
+        {
+            openTitleScreen();
+        }
     }
     
+    /**
+     * Plays the game.
+     */
     private static void playGame()
     {
         Level level = new Level();
         level.play();
     }
     
+    /**
+     * Opens the title screen which has a button to play the game.
+     */
     private static void openTitleScreen()
     {
         final JFrame window = new JFrame("Super Cool Game");
@@ -31,9 +58,6 @@ public class Game
             }
         });
         window.add( playButton );
-        
-        
-        
         window.setVisible( true );
     }
 
