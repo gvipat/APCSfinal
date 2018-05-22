1ded6d35d6379c20f4d2fcd19848127d26e9b0bf
yvonneli <yvonneli@10.0.0.136>
2018-05-21 23:11:27 -0700

    designed new level

--- Level.java
+++ Level.java
@@ -27,19 +27,32 @@ public class Level
     /**
      * Contains the data for all level, each row represents one level's sprites.
      */
-    private static final Sprite[][] sprites =
+    private static final Sprite[][] sprites = //found glitch: if fall down the wall 2 and hold left, it acts as if it lands on the ground tho mid air
 
                     { { new PlayerSprite( 5, 5, 20, 20, Color.BLUE ),
-                        new GroundSprite( 0, 400, 1000, 40 ),
-                        new GroundSprite( 300, 240, 2000, 40 ),
-                        new GroundSprite( 400, 360, 2000, 40 ),
+                        new GroundSprite( 0, 400, 100, 40 ),//floor1
+                        new GroundSprite( 300, 400, 30, 40 ),//hanging floor
+                        new GroundSprite( 400, 400, 450, 40 ),//floor2
+                        new GroundSprite( 920, 170, 300, 40 ),//floor3
+                        new GroundSprite( 1180, 400, 300, 40 ),//floor4
+                        new GroundSprite( 1480, 360, 200, 100 ),//floor5
+                        new GroundSprite( 1680, 300, 200, 160 ),//floor6
+                        new GroundSprite( 1880, 240, 200, 220 ),//floor7
+                        //new GroundSprite( 100, 360, 50, 40 ),//wall 1
+                        new GroundSprite( 750, 340, 50, 60 ),//step
+                        new GroundSprite( 800, 240, 50, 160 ),//wall 2
+                        new GroundSprite( 1180, 210, 40, 190 ),//wall 3
+                        //new GroundSprite( 550, 400, 500, 40 ),
+                        new EnemySprite( 710, 100, 20, 20, Color.RED ),
+                        new EnemySprite( 750, 100, 20, 20, Color.RED ),
+                        new EnemySprite( 780, 100, 20, 20, Color.RED ),
                         // new GroundSprite(300, 180, 20, 20),
-                        new PacingEnemySprite( 1500, 100, 20, 20, Color.ORANGE, 50 ),
-                        new EnemySprite( 1200, 100, 20, 20, Color.RED ),
-                        new EnemySprite( 1300, 100, 20, 20, Color.RED ),
-                        new EnemySprite( 1400, 100, 20, 20, Color.RED ),
-                        new EnemySprite( 1600, 100, 20, 20, Color.RED ),
-                        new EnemySprite( 1700, 100, 20, 20, Color.RED ) },
+                        //new PacingEnemySprite( 1500, 100, 20, 20, Color.ORANGE, 50 ),
+                        
+                        //new EnemySprite( 1300, 100, 20, 20, Color.RED ),
+                        //new EnemySprite( 1400, 100, 20, 20, Color.RED ),
+                        //new EnemySprite( 1600, 100, 20, 20, Color.RED ),
+                        /*new EnemySprite( 1700, 100, 20, 20, Color.RED )*/ },
                         { new PlayerSprite( 200, 300, 20, 20, Color.ORANGE, "player" ),
                             new GroundSprite( 0, 400, 1000, 40, "floor" ),
                             new GroundSprite( 400, 280, 500, 120, "hanging 1" ) } };
