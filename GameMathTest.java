749ca956f7df47eac837dc0e3d0d335bff0ea2d7
Gaurav Vipat <gaurav.vipat@gmail.com>
2018-05-22 14:55:44 -0700

    remov game math and ground sprite tests

--- GameMathTest.java
+++ GameMathTest.java
@@ -6,13 +6,16 @@ class GameMathTest {
 
     @Test
     void roundToHundredths() {
+	    assertEquals(GameMath.roundToHundredths(0.000000007), 0.0)
     }
 
     @Test
     void roundToTenths() {
+	assertEquals(GameMath.roundToHundredths(0.000000007), 0.0)
     }
 
     @Test
     void getSign() {
+	    assertEquals(-1, GameMath.getSign(-5));
     }
 }
--- GroundSpriteTest.java
+++ GroundSpriteTest.java
@@ -4,7 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GroundSpriteTest {
 
+	Sprite testS = new Sprite();
+	SPtire testS2 = testS.copy();
+
     @Test
     void copy() {
+	    assertNotNull(testS2);
+	    assertEqual(testS, testS2);
     }
 }
